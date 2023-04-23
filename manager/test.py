3deb4bf544f192247d4673f59e38d1e12e8509df
Myoko <92731952+MyokoJP@users.noreply.github.com>
2023-04-23 13:10:21 +0900

    Delete test.py

--- manager/test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-console = "aa [10:53:07 INFO]: Total players online: 0"
-if re.findall(r'\d+', console):
-    if re.findall(r'\d+', console):
-        players_online = int(re.findall(r'\d+', console)[-1])
-        print(players_online)
